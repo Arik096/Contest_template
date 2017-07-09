448ecac8d33fbf4cdc3bd7b8a37e07cdcff831d7
Arik Md. Isthiaque <arikbncc@gmail.com>
2017-07-09 21:02:31 +0600

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,83 @@
+///template
+
+///Header file starts here
+#include <bits/stdc++.h>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cmath>
+#include <iterator>
+#include <cstdio>
+#include <cstdlib>
+#include <cctype>
+#include <climits>
+#include <string>
+#include <sstream>
+#include <cstring>
+#include <ctime>
+///Header File ends here
+
+
+///Macro starts here
+#define pf printf
+#define sf scanf
+#define sfutest scanf("%llu",&n)
+#define sftest scanf("%lld",&n)
+
+#define ulld unsigned long long int
+#define lld  long long int
+#define ullf unsigned long long double
+#define llf  long long double
+
+#define wh while
+#define br break
+#define con continue
+#define sw switch
+#define el else
+#define eli else if
+#define pro protected
+#define pub public
+#define pri private
+#define vd void
+#define cl class
+#define sh show
+#define disp display
+#define op operator
+#define inl inline
+#define vir virtual
+#define fr friend
+#define tr true
+#define fl false
+
+#define nl printf("\n")
+#define tab printf("\t")
+#define space printf(" ")
+
+#define MOD              1000000007
+#define gamma 0.57721566490153286060651209008240243
+
+#define luv_u return
+#define null -1
+
+#define testloop0 for(i=0;i<n;i++)
+#define testloop1 for(i=1;i<=n;i++)
+///Macro Ends here
+
+
+int main()
+{
+          tab;
+          pf("hello friend");
+          nl;
+          luv_u 0;
+}
